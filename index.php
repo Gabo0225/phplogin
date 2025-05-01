198278fcfa804174b32fdb076afe0f528f894e10
Gabo0225 <gabrielsaher@hotmail.com>
2025-04-30 19:51:05 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+// Apartado para incluir la vista en html //
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/CSS/styles..css">
+    <title>Login / Singup PHP</title>
+</head>
+<body>
+    <h1>Bienvenido por favor registrate o incia sesion en la web</h1>
+    <div id="enlaces">
+        <a href="/login.php">acceda aqui</a>
+        <a href="/singup.php">Registrate aqui</a>
+    </div>
+    
+</body>
+</html>
